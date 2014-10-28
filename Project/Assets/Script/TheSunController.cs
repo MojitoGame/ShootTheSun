bee84de5ab072c0b4d8fdf5f84308d6059b0820d
ianyzhang <silverlight_xd@qq.com>
2014-10-28 23:29:22 +0800

    基础物理功能添加

---
+++ Project/Assets/Script/TheSunController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TheSunController : MonoBehaviour
+{
+	// Use this for initialization
+	void Start () 
+	{
+		this.rigidbody2D.AddForce(new Vector2(100.0f,300.0f));
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	}
+}
